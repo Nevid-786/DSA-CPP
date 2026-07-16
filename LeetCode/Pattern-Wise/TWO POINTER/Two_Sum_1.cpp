38418f2c578fd7d9325a89d4e4cf1012411c512d
Nevid-786 <mdnabid786@gmail.com>
2026-07-16 19:27:58 +0530

    leet 1:Two sum (value return)

--- LeetCode/Pattern-Wise/TWO POINTER/Two_Sum_1.cpp
+++ LeetCode/Pattern-Wise/TWO POINTER/Two_Sum_1.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum_Value(vector<int>& nums, int target) {
         vector <int> temp;
         sort(nums.begin(),nums.end());
         int n= nums.size();
@@ -27,13 +27,36 @@ public:
         return temp;
         
     }
+
+
+      vector<int> twoSum_indexes(vector<int>& nums, int target) {
+        vector<int> temp;
+        int found=0;
+
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (nums[i] + nums[j] == target) {
+                    temp.push_back(i);
+                    temp.push_back(j);
+                    found=1;
+                    break;
+                }
+            }
+            if(found==1){
+              break;
+            }
+        }
+       return temp;
+    }
 };
 
 int main() {
     vector<int> v={1,4,6,2,4,42,3};
     int target=44;
     Solution s;
-    v=s.twoSum(v,target);
+    // v=s.twoSum_Value(v,target);
+    v=s.twoSum_Value(v,target);
    for(auto it:v){
     cout<<it<<",";
    }
