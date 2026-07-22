9b77c66c83f645aef870e9c2521ff432da7babda
Nevid-786 <mdnabid786@gmail.com>
2026-07-22 23:09:57 +0530

    Rearrange 0s and 1s:GFG

---
+++ LeetCode/Pattern-Wise/TWO POINTER/Rearrange_0_and_1.cpp
@@ -0,0 +1,47 @@
+// Given an array arr[] consisting of only 0's and 1's.
+// Modify the array in - place to segregate 0s onto the left side and 1s onto the right side of the array.
+
+//    Examples :
+
+//     Input : arr[] = [ 0, 1, 0, 1, 0, 0, 1, 1, 1, 0 ] 
+//     Output : [ 0, 0, 0, 0, 0, 1, 1, 1, 1, 1 ] 
+// https://www.geeksforgeeks.org/problems/segregate-0s-and-1s5106/1
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+        int Zeros=0;
+        int Ones=0;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                Zeros++;}
+            else{
+                Ones++;
+            }
+        }
+        arr.clear();
+  
+        for(int i=0;i<Zeros;i++){
+            arr.push_back(0);
+        }
+        
+        for(int i=0;i<Ones;i++){
+            arr.push_back(1);
+        }
+        
+    }
+};
+int main() {
+    Solution s;
+    vector<int> a={0, 1, 0, 1, 0, 0, 1, 1, 1, 0};
+  
+    s.segregate0and1(a);
+  for(int i=0;i<a.size();i++){
+      cout<<a[i]<<" ";
+    }
+    return 0;
+}
