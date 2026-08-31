4708cc664e3e7f46ac855a0420392ad374ae71f2
Nevid <mdnabid786@gmail.com>
2026-08-31 10:23:41 +0530

    Time: 478 ms (8.65%), Space: 44.8 MB (53.48%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -4,24 +4,53 @@ public:
 
         int n = nums.size();
         int count = 0;
-        int l = 0;
-
-        while (l < n) {
-            int h = l;
+       int l = 0;
+            int h = 0;
             int sum =1;
 
-            while (h < n) {
-                sum *= nums[h];
-                if (sum < k) {
-                    count++;
-                }else {
-                    break;
-                }
-           
-              h++;  
+        while ( h < n) {
+            sum*=nums[h];
+            
+            while (sum >=k && l<n ) {
+                cout<<sum<<endl;
+                 sum/=nums[l]; 
+                l++;
             }
-            l++;
+            count+=h-l+1;
+           
+            h++;
         }
         return count;
     };
 };
+
+
+
+//Brute force T:O(n2)
+// class Solution {
+// public:
+//     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+
+//         int n = nums.size();
+//         int count = 0;
+//         int l = 0;
+
+//         while (l < n) {
+//             int h = l;
+//             int sum =1;
+
+//             while (h < n) {
+//                 sum *= nums[h];
+//                 if (sum < k) {
+//                     count++;
+//                 }else {
+//                     break;
+//                 }
+           
+//               h++;  
+//             }
+//             l++;
+//         }
+//         return count;
+//     };
+// };
