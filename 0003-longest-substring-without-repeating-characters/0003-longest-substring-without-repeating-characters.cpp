8b5d7333d3bca9593c67d8795c3dd1b27edd9b5c
Nevid <mdnabid786@gmail.com>
2026-08-31 22:11:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+         map<char,int>m;
+         if(s.size()<1) return 0;
+        int l=0;
+        int h=0;
+        int res=INT_MIN;
+        int length;
+        while(h<s.size()){
+            m[s[h]]++;
+            length=h-l+1;
+            cout<<length<<endl;
+            
+            while(length>m.size()){
+                // cout<<"l:"<<l <<endl;
+                m[s[l]]--;
+                if(m[s[l]]<=0){
+                    m.erase(s[l]);
+                }
+                l++;
+                length=h-l+1;
+            }
+            res=max(res,h-l+1); // as window have only 2 fruits therfore its lenth is equal to no. of fruits in window 
+            h++;
+        }
+      return res==INT_MIN?1:res;
+        
+    }
+    
+};
