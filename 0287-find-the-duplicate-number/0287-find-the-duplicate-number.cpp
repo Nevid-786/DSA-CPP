84f0575451e020fab363dc38cb47fb9cdd000a2c
Nevid <mdnabid786@gmail.com>
2026-09-22 21:10:18 +0530

    Time: 13 ms (39.54%), Space: 65.1 MB (69.71%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,18 +1,48 @@
+//using slow and fast pointer
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        if(n==1){
-            return nums[0];
+        
+      int s,f;
+      s=f=0;
+      while(true){
+        s=nums[s];
+        f=nums[nums[f]];
+        if(s==f){
+           s=0;
+           while(s!=f){
+             s=nums[s];
+        f=nums[f];
+           } 
+           return s;
         }
-
-        for(int i=0;i<n;i++){
-            if(nums[i]==nums[i+1]){
-                return nums[i];
-            }
-        }
-        return -1;
+      }
+      return -1;
         
     }
 };
+
+// Using Sorting and returning first duplicate
+// Time comp:O(nlogn);
+// space comp:O(1);
+
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         int n=nums.size();
+//         if(n==1){
+//             return nums[0];
+//         }
+
+//         for(int i=0;i<n;i++){
+//             if(nums[i]==nums[i+1]){
+//                 return nums[i];
+//             }
+//         }
+//         return -1;
+        
+//     }
+// };
+
